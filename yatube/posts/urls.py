90ed4a6173e3d14914d97cc190ac3d79c9dcc163
Екатерина Василевич <katerinair8@mail.ru>
2022-08-03 21:44:26 +0200

    Коммит перед ревью

---
+++ yatube/posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    path('group/<slug>/', views.group_posts, name='group_posts'),
+    path('', views.index, name='index'),  
+]
